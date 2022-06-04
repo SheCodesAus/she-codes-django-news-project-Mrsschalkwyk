dae87c7300d20003de5af66ba0db56429ab7a6dd
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-04 19:07:42 +1000

    ok

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -4,5 +4,5 @@ from .views import CreateAccountView
 app_name = 'users'
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(),
-    name='createAccount'),
-    ]
+name='createAccount'),
+]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import generic 
-#from .models import CustomUser 
+from .models import CustomUser 
 from .forms import CustomUserCreationForm
 
 class CreateAccountView(CreateView):
