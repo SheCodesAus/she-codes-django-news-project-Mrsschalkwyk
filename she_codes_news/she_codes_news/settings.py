28a461771aa49ecb711a9f836241570f03813eb2
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-11 10:59:48 +1000

    heroku

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get('DJANGO_DEBUT') !='False'
 
 ALLOWED_HOSTS = ['*']
 
-db_from_env = dj_database_url.config(conn_max_age=500)DATABASES['default'].update(db_from_env)
+
 
 
 # Application definition
@@ -51,6 +51,7 @@ LOGOUT_REDIRECT_URL = 'news:index'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -89,7 +90,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
