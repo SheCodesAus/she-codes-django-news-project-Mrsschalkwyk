9537bf6718b3c26f2a684e4a994ae2383ef98269
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-14 09:38:20 +1000

    url

--- she_codes_news/users/admin.py
+++ she_codes_news/users/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-
-# Register your models here.
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+# Register your models here.
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
