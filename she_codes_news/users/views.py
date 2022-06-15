7b9487c5bf3a26d312f23def19e3a75f61f68875
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-15 21:44:09 +1000

    install requirements

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -6,8 +6,7 @@ from django.views.generic.edit import CreateView
 from django.views import generic 
 from .models import CustomUser 
 from .forms import CustomUserCreationForm
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 
 
 
