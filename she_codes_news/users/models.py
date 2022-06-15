f0be8fac26e13b89858f935ccdfa3f5a3184bd11
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-15 10:06:10 +1000

    images

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+
 # Create your models here.
 class CustomUser(AbstractUser):
     pass
     def __str__(self):
         return self.username
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
+#     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+#     def __str__(self):
+#         return f'{self.user.username} Profile' #show how we want it to be displayed
