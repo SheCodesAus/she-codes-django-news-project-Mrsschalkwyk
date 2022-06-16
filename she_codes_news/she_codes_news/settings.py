4e0210891195f4de3bdb0df9c8ca8de4eeec4a55
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-06-16 11:28:11 +1000

    Update settings.py

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUT') !='False'
+DEBUG = os.environ.get('DJANGO_DEBUT') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
